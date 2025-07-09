614868b173bb2bfb744421221ae33c9d51144f70
Antônio Conceição <antonio.conceicao97@hotmail.com>
2025-07-08 23:33:27 -0300

    v0.04

---
+++ Novo/cursoemvideo/ded.py
@@ -0,0 +1,10 @@
+import os
+os.startfile ('music.mp3')
+
+
+n1 = float (input('Olá, guerreiro(a) diga o seu nome e vamos derrotar o dragão '))
+
+n2 = float (input (f'OK, {n1} , onde voce quer atacar primeiro \n 1 - na cabeça {} \n 2- no coração {} \n 3 - na calda do dragão {}' ))
+
+
+
