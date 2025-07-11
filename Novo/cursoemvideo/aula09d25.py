4d6757790890d7d194b2f2aec8e72bad37da24dd
Antônio Conceição <antonio.conceicao97@hotmail.com>
2025-07-11 18:28:50 -0300

    v0.07

---
+++ Novo/cursoemvideo/aula09d25.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome.
+
+text = input('Digite o nome da cidade e descubra se ela tem Santo ')
+pequeno = text.lower()
+print ('silva' in pequeno)
