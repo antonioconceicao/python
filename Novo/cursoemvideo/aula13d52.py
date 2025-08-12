3f4a8a55ae6077fb2937b7d2dc19ca3f7f16692d
Antônio Conceição <antonio.conceicao97@hotmail.com>
2025-08-11 23:16:48 -0300

    v1.04

--- Novo/cursoemvideo/aula13d52.py
+++ Novo/cursoemvideo/aula13d52.py
@@ -1,1 +1,12 @@
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+numero = int (input('Digite um número '))
+total = 0
+for c in range (1 ,numero + 1):
+    if numero % c == 0:
+        total += 1
+
+if total == 2:
+        print ('Esse número é primo')
+else:
+        print ('Esse não é número primo')
--- Novo/cursoemvideo/aula13d53.py
+++ Novo/cursoemvideo/aula13d53.py
@@ -4,3 +4,14 @@
 #A torre da derrota
 #O lobo ama o bolo
 #Anotaram a data da maratona
+
+frase = input ('Digite uma frase ')
+cortar = "".join(frase.split())
+diminuir = cortar.lower()
+palindromo = diminuir[::-1]
+
+if palindromo == diminuir:
+    print(f'a frase "{frase}" invertido fica {palindromo}. \n Essa frase é um Palíndromo')
+else:
+    print (f'a frase "{frase}" invertido fica {palindromo}. \n Essa frase NÃO é um Palíndromo')
+
