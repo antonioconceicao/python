d621179ea88cdfd892315e732f82f104adec08c1
Antônio Conceição <antonio.conceicao97@hotmail.com>
2025-08-14 22:35:30 -0300

    v1.05

--- Novo/cursoemvideo/aula13d54.py
+++ Novo/cursoemvideo/aula13d54.py
@@ -1,1 +1,15 @@
 #Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantaas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+import datetime
+ano = []
+for idade in range(1, 8):
+    nascimento = int (input(f'Digite o {idade} ano do seu aniversário '))
+    dataexata = datetime.date.today().year
+    conta = dataexata - nascimento
+    ano.append(conta)
+    adulto = [idade for idade in ano if idade >=18]
+    jovem = [idade for idade in ano if idade < 18]
+    
+print(f'Ao todo tivemos {len(adulto)} pessoas maiores de idade \n e também tivemos {len(jovem)} pessoas menores de idade')
+
+
+
--- Novo/cursoemvideo/aula13d55.py
+++ Novo/cursoemvideo/aula13d55.py
@@ -1,1 +1,6 @@
 #Faça um programa que leia o peso de cinco pessoas.No final, mostre qual foi o maior e o menor peso lidos.
+peso = []
+for verificador in range(1, 6):
+    pessoas = float (input (f'Digite o {verificador} peso da pessoa '))
+
+print(pessoas)
