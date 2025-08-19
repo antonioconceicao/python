cecb9bbff9ed2f5df9f65208d462ecd9f95bb7d5
Antônio Conceição <antonio.conceicao97@hotmail.com>
2025-08-18 22:09:58 -0300

    v1.06

--- Novo/cursoemvideo/aula13d55.py
+++ Novo/cursoemvideo/aula13d55.py
@@ -1,6 +1,8 @@
 #Faça um programa que leia o peso de cinco pessoas.No final, mostre qual foi o maior e o menor peso lidos.
 peso = []
 for verificador in range(1, 6):
-    pessoas = float (input (f'Digite o {verificador} peso da pessoa '))
-
-print(pessoas)
+    pessoas = float (input (f'Digite o {verificador} peso da pessoa. Digite apenas números inteiros '))
+    peso.append(pessoas)
+minimo = min(peso)
+maximo = max(peso)
+print(f'O maior peso é {maximo} e o menor é {minimo}')
