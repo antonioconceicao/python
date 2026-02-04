722d177e9877a50125c2e8f9bacac8227e90f1fb
Antônio Conceição <antonio.conceicao97@hotmail.com>
2026-02-04 20:54:34 -0300

    v1.08.1

--- Novo/cursoemvideo/aula14d64.py
+++ Novo/cursoemvideo/aula14d64.py
@@ -1,1 +1,18 @@
 #Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles.(Desconsiderando o flag).
+
+numeros = 0
+quantidade = 0
+
+
+print ('Bem-vindo! Começa a usar o nosso programa! \n para encerrar o programa digite 999')
+
+while True:
+    digitar = int (input('Digite um valor: '))
+
+    if digitar == 999:
+        fim = input(f'Você digitou 999 e o programa vai ser encerrado! \n Você digitou {quantidade} números. \n A conta total dos números digitado foi: {numeros}  ')
+        break
+    numeros += digitar
+    quantidade += 1
+    
+    
--- Novo/cursoemvideo/aula14d65.py
+++ Novo/cursoemvideo/aula14d65.py
@@ -1,1 +1,15 @@
 #Crie um programa que leia vários números inteiros pelo teclado.No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+valores = 0
+
+print('Bem-vindo! Obrigado por usar o Tabajara Inc! O programa vai começar agora!')
+
+while = True:
+    print ('Para terminar o programa digite a letra Q')
+    digite = int(input('Digite um valor: '))
+    sair = input ('Caso queira sair aperter Q ')
+    if sair == q:
+        print('O programa foi encerrado!')
+    else:
+        digite = ('Digite um valor: ')
+
