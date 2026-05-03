1c90e6695ae068a39e273db1f09c9a5c01bad9f3
Antônio Conceição <antonio.conceicao97@hotmail.com>
2026-05-03 18:50:48 -0300

    v1.08.2

--- Novo/cursoemvideo/aula14d65.py
+++ Novo/cursoemvideo/aula14d65.py
@@ -1,15 +1,29 @@
 #Crie um programa que leia vários números inteiros pelo teclado.No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
 valores = 0
+guardarvalores = []
+media = 0
 
-print('Bem-vindo! Obrigado por usar o Tabajara Inc! O programa vai começar agora!')
+print('Bem-vindo! Obrigado por usar o Tabajara Inc! O programa vai começar agora!\n Para terminar o programa digite a letra 999')
 
-while = True:
-    print ('Para terminar o programa digite a letra Q')
-    digite = int(input('Digite um valor: '))
-    sair = input ('Caso queira sair aperter Q ')
-    if sair == q:
-        print('O programa foi encerrado!')
-    else:
-        digite = ('Digite um valor: ')
+contador = 0
 
+while True :
+    sequencia = int(input('Digite um valor: '))
+    
+    if sequencia == 999:
+        print(' O programa foi encerrado! ')
+        break
+    valores += sequencia
+    contador += 1
+    guardarvalores.append(sequencia)
+
+
+
+
+media = valores / contador
+minimo = min(guardarvalores)
+maximo = max(guardarvalores)
+
+
+print(f'O resultado da media é {media} \n O menor valor é {minimo} \n O maior número é {maximo}')
